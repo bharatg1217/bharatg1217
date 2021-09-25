c135677493d384c94a14a29d31b1bc0fecda6491
bharatg1217 <91063068+bharatg1217@users.noreply.github.com>
2021-09-25 19:34:24 +0530

    Create about.blade.php

---
+++ resources/views/sections/about.blade.php
@@ -0,0 +1,20 @@
+<section id="about">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6">
+        <h2>About The Event</h2>
+        <p>It is an excellent opportunity to join a growing community of educators, leaders, scholars, and learners. We are united to promote change through innovation in health professions education, working together toward high levels of excellence and wellbeing.</p>
+      </div>
+      <div class="col-lg-3">
+        <h3>When</h3>
+        <p>Friday to Sunday<br>10-12 December</p>
+
+        
+      </div>
+      <div class="col-lg-3">
+        <h3>Where</h3>
+        <p>Hitex Exhibition Center,Hyderabad,India</p>
+      </div>
+    </div>
+  </div>
+</section>
