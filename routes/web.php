41415a7426d72a6b6c13e99d442679f11a7206fb
bharatg1217 <91063068+bharatg1217@users.noreply.github.com>
2021-09-25 19:28:05 +0530

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,61 @@
+<?php
+
+Route::get('/', 'HomeController@index')->name('home');
+Route::get('speaker/{speaker}', 'HomeController@view')->name('speaker');
+Route::redirect('/home', '/admin');
+Auth::routes(['register' => true]);
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'middleware' => ['auth']], function () {
+    Route::get('/', 'HomeController@index')->name('home');
+    // Permissions
+    Route::delete('permissions/destroy', 'PermissionsController@massDestroy')->name('permissions.massDestroy');
+    Route::resource('permissions', 'PermissionsController');
+
+    // Roles
+    Route::delete('roles/destroy', 'RolesController@massDestroy')->name('roles.massDestroy');
+    Route::resource('roles', 'RolesController');
+
+    // Users
+    Route::delete('users/destroy', 'UsersController@massDestroy')->name('users.massDestroy');
+    Route::resource('users', 'UsersController');
+
+    // Settings
+    Route::delete('settings/destroy', 'SettingsController@massDestroy')->name('settings.massDestroy');
+    Route::resource('settings', 'SettingsController');
+
+    //Tickets
+    Route::delete('tickets/destroy', 'TicketsController@massDestroy')->name('tickets.massDestroy');
+    Route::resource('tickets', 'TicketsController');
+
+    // Speakers
+    Route::delete('speakers/destroy', 'SpeakersController@massDestroy')->name('speakers.massDestroy');
+    Route::post('speakers/media', 'SpeakersController@storeMedia')->name('speakers.storeMedia');
+    Route::resource('speakers', 'SpeakersController');
+
+    // Schedules
+    Route::delete('schedules/destroy', 'ScheduleController@massDestroy')->name('schedules.massDestroy');
+    Route::resource('schedules', 'ScheduleController');
+
+    // Venues
+    Route::delete('venues/destroy', 'VenuesController@massDestroy')->name('venues.massDestroy');
+    Route::post('venues/media', 'VenuesController@storeMedia')->name('venues.storeMedia');
+    Route::resource('venues', 'VenuesController');
+
+    // Galleries
+    Route::delete('galleries/destroy', 'GalleriesController@massDestroy')->name('galleries.massDestroy');
+    Route::post('galleries/media', 'GalleriesController@storeMedia')->name('galleries.storeMedia');
+    Route::resource('galleries', 'GalleriesController');
+
+    // Sponsors
+    Route::delete('sponsors/destroy', 'SponsorsController@massDestroy')->name('sponsors.massDestroy');
+    Route::post('sponsors/media', 'SponsorsController@storeMedia')->name('sponsors.storeMedia');
+    Route::resource('sponsors', 'SponsorsController');
+
+    // Amenities
+    Route::delete('amenities/destroy', 'AmenitiesController@massDestroy')->name('amenities.massDestroy');
+    Route::resource('amenities', 'AmenitiesController');
+
+    // Prices
+    Route::delete('prices/destroy', 'PricesController@massDestroy')->name('prices.massDestroy');
+    Route::resource('prices', 'PricesController');
+});
