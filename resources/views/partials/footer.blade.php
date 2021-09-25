a82e166e890fd845f0233dc6024cfd12579ade79
bharatg1217 <91063068+bharatg1217@users.noreply.github.com>
2021-09-25 19:35:38 +0530

    Create footer.blade.php

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,10 @@
+<footer id="footer">
+  <div class="container">
+    <div class="copyright">
+      &copy; Copyright <strong>{{ env('APP_NAME', 'TheEvent') }}</strong>. All Rights Reserved
+    </div>
+    <div class="credits">
+      Designed by Bharat using <a target ="_blank" href="https://bootstrapmade.com/">BootstrapMade</a>
+    </div>
+  </div>
+</footer><!-- #footer -->
